bedda8abc56db80ce1538fda0cdbb368846af390
Evans Winner <ego111@gmail.com>
2014-06-28 00:05:55 -0700

    Generalize Euler 2 slightly

--- project-euler/2.lisp
+++ project-euler/2.lisp
@@ -1,6 +1,5 @@
-;; Project Euler number 2.  I am not sure if this is a recursive
-;; solition but I'm not sure actually what the space profile of this
-;; function is.  
+;; Project Euler number 2.  This is a recursive solition but I'm not
+;; sure actually what the space profile of this function is.
 
 (defun fib-list (upper-bound &optional (list '(1 2)))
   (if (>= (car (last list)) upper-bound)
@@ -11,7 +10,7 @@
 	       (list (+ (car (last list))
 			(car (last list 2))))))))
 
-(defun euler-2 ()
-  (reduce #'+ (remove-if #'oddp (butlast (fib-list 4000000)))))
+(defun euler-2 (upper-bound)
+  (reduce #'+ (remove-if #'oddp (butlast (fib-list upper-bound)))))
 
-;; (euler-2) ==> 4613732
+;; (euler-2 4000000) ==> 4613732
