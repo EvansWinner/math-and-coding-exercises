5c6bbf28f89b03e93f0f758057083ed51647a2bb
Evans Winner <evans.winner@gmail.com>
2021-02-07 19:11:51 -0700

    Oops -- forgot string test case. Haskell is absurd.

--- 99haskell_1.hs
+++ 99haskell_1.hs
@@ -3,4 +3,7 @@ mylast [x] = x
 mylast (x:xs) = mylast xs
 
 main :: IO ()
-main = print (mylast [1,2,3,4,5])
+main = do 
+  print (mylast [1,2,3,4])    -- Should be 4
+  print (mylast ['x','y','z']) -- Should be 'z'
+
