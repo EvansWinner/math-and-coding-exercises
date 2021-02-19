70902c4612218862c15876cf59094afe69f4207c
Evans Winner <evans.winner@gmail.com>
2021-02-18 20:22:09 -0700

    One more test case

--- praxis_local-maxima.py
+++ praxis_local-maxima.py
@@ -12,10 +12,11 @@ def lmax(lst:List[int])->List[int]:
     if i==len(l)-1 and l[i-1]<n:ret.append(n);continue
     if n>l[i-1] and n>l[i+1]:ret.append(n)
   return ret
-def test_lmax1():
-  assert lmax([1,2,2,3,3,2,3])==[3,3]#test duplicates
-def test_lmax2():
+def test_lmax_duplicates():
+  assert lmax([1,2,2,3,3,2,3])==[3,3]
+def test_lmax_praxis_test_case():
   assert lmax([1,2,1,2,3,4,3,2,3,2,1])==[2,4,3]
-
+def test_lmax_beginning():
+  assert lmax([4,2,2,3,4,5])==[4,5]
 
 
